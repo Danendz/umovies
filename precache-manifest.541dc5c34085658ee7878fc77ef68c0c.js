0d5eb8304ad22070648f75f959abf509f77fecf5
Danendz <danilapayne@gmail.com>
2020-04-08 21:37:46 +0500

    Updates

---
+++ precache-manifest.541dc5c34085658ee7878fc77ef68c0c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1c170dab169cca59f5ebd48ca9067d",
+    "url": "/umovies/index.html"
+  },
+  {
+    "revision": "3570edf662a619ee086a",
+    "url": "/umovies/static/css/2.66165e39.chunk.css"
+  },
+  {
+    "revision": "882e6f87b49b26962f62",
+    "url": "/umovies/static/css/main.f2494616.chunk.css"
+  },
+  {
+    "revision": "3570edf662a619ee086a",
+    "url": "/umovies/static/js/2.3de4fb53.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/umovies/static/js/2.3de4fb53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882e6f87b49b26962f62",
+    "url": "/umovies/static/js/main.e100c253.chunk.js"
+  },
+  {
+    "revision": "d74be15cac58be2e6497",
+    "url": "/umovies/static/js/runtime-main.5e210e1b.js"
+  },
+  {
+    "revision": "57982102a6a9948b2d0c8d124bb1dbd8",
+    "url": "/umovies/static/media/avengers.57982102.jpg"
+  },
+  {
+    "revision": "927c17b90bd6dbb2a7eda548e32dbdfa",
+    "url": "/umovies/static/media/avengersW.927c17b9.jpg"
+  },
+  {
+    "revision": "d3b52e531ec00c2a128975a634538cf7",
+    "url": "/umovies/static/media/fast9.d3b52e53.jpg"
+  },
+  {
+    "revision": "359d205dc718f80ab850e0cbb2bd5329",
+    "url": "/umovies/static/media/jokerC.359d205d.jpg"
+  },
+  {
+    "revision": "d34d86612487333d0ff96fc0fa8b6406",
+    "url": "/umovies/static/media/pirates.d34d8661.jpg"
+  },
+  {
+    "revision": "94edba74785d696da56c3d212b2e2b59",
+    "url": "/umovies/static/media/woman.94edba74.jpg"
+  }
+]);
